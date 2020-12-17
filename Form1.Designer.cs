9ca52db600c3c8b99b293654aba78cac703cf48d
исп-31 <-31@LAB17-06>
2020-12-17 14:06:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Практическая_работа__16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.оПргограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(238, 278);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(143, 129);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(79, 63);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(78, 78);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Практическая_работа__16.Properties.Resources.winter;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(625, 499);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.оПргограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(625, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.menuStrip1_KeyUp);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // оПргограммеToolStripMenuItem
+            // 
+            this.оПргограммеToolStripMenuItem.Name = "оПргограммеToolStripMenuItem";
+            this.оПргограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПргограммеToolStripMenuItem.Text = "О программе";
+            this.оПргограммеToolStripMenuItem.Click += new System.EventHandler(this.оПргограммеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 523);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem оПргограммеToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая_работа__16
+{
+    public partial class Form1 : Form
+    {
+        int veter, vector, counter;
+        Rectangle r1, r2;
+
+
+        private void menuStrip1_KeyUp(object sender, KeyEventArgs e)
+        {
+            vector = 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)//если нажата кнопка влево
+                vector = -10;//вектор смещения влево
+            if (e.KeyData == Keys.Right)//если нажата кнопка вправо
+                vector = 10;//вектор смещения вправо 
+        }
+
+        private void оПргограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработчик Алешина Дарья ИСП-31" +
+                "Игра Снегопад");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.Width = 639;
+            this.Height = 479;
+            this.MaximizeBox = false;// кнопка развёртывая
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            pictureBox2.Height = 60;//высота
+            pictureBox2.Width = 80;//ширина         
+            pictureBox5.Parent = pictureBox1;
+            pictureBox2.Parent = pictureBox1;
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox2.BackColor = Color.Transparent;
+            Random r = new Random();//рандом
+            //задаем расположение объекта по координате Х от 100 д 200
+            pictureBox2.Left = r.Next(50, 400);
+            //задаем расположения объекта  по координате y
+            pictureBox2.Top = -120;
+            veter = r.Next(5);//задаем силу ветра
+            vector = 0;//обнуляем скорость смещения
+            counter = 1;//счетчик для отображения картинок
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            //задаем расположение объекта 
+            pictureBox5.Left = r.Next(50, 400);
+
+            //задаем расположения объекта 
+            pictureBox5.Top = -120;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int skorost_padenia = 5;//скорость падения
+
+            //перемещение по X с учетом ветра и вектора управления
+            pictureBox2.Left = pictureBox2.Left + veter + vector;
+
+            //Перемещение по Y
+            pictureBox2.Top = pictureBox2.Top + skorost_padenia;
+
+            counter++;//Увеличиваем значение
+            if (counter > 10)
+                counter = 0;
+
+            //Картинка вышла за форму
+            if (pictureBox2.Top >= pictureBox1.Height)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Проигрыш");
+            }
+            r1 = pictureBox2.DisplayRectangle; r2 = pictureBox5.DisplayRectangle;
+            r1.Location = pictureBox2.Location; r2.Location = pictureBox5.Location;
+
+            //Картинка пересеклась с другой картинкой
+            if (r1.IntersectsWith(r2) == true)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Вас засыпало снегом. Поздравляю!");
+            }
+        }
+    }
+}
